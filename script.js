df68bcc8ecb5835242851738ca513fffed319bc5
Caio0-0 <126476942+Caio0-0@users.noreply.github.com>
2023-05-04 13:51:36 -0300

    atualização

---
+++ script.js
@@ -0,0 +1,16 @@
+const menu = document.querySelector('.menu')
+const fecharMenu = document.querySelector(".fechar-menu")
+const abrirMenu = document.querySelector('.abrir-menu')
+
+
+abrirMenu.addEventListener("click",show)
+fecharMenu.addEventListener("click",fechar)
+
+function show(){
+    menu.style.display = 'flex'
+    menu.style.top = '0'
+
+}
+function fechar(){
+    menu.style.top ='-100%'
+}
